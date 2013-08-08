a022c82b0cf54804bad6b8d69e8441c5951c551c
Naoufal Kadhom <naoufalkadhom@gmail.com>
2013-08-08 01:03:15 -0400

    organize js.  up next setup transoform3d animations

--- nkProgressHUD.js
+++ nkProgressHUD.js
@@ -10,66 +10,81 @@
  *
  */
 
-var windowHeight;
-var nkProgressHUDWrapperHeight;
-var nkProgressHUDWrapperStart;
-var nkProgressHUDWrapperEnd;
 
-//default string is 'Loading'
-function showNKProgressHUD(loaderString) {  
-	//append html
-	$('body').append("<div class='nkProgressHUDPage'><div class='nkProgressHUDWrapper'><div class='nkProgressHUDSpinner'></div><div class='nkProgressHUDText'>Loading</div><div class='clear'></div></div></div>");
 
-	// set heights to vertically align HUD
-	nkProgressHUDWrapperHeight = $('.nkProgressHUDWrapper').height();	
-	nkProgressHUDWrapperStart = ((windowHeight - nkProgressHUDWrapperHeight) / 2) - ((windowHeight / 100) * 5);
-	nkProgressHUDWrapperEnd = nkProgressHUDWrapperStart + 50;
-	$('.nkProgressHUDWrapper').css('margin-top', nkProgressHUDWrapperEnd);
+// Declare Namespace
+var nkProgressHUD = {};
 
-	//start animation
-	$('.nkProgressHUDWrapper').show();
-	$('div.nkProgressHUDText').html(loaderString)
-	$('.nkProgressHUDWrapper').animate({marginTop: nkProgressHUDWrapperStart,}, {duration: 1000,queue: false});
-	$('.nkProgressHUDPage').fadeIn();
+
+
+// Declare Variables
+nkProgressHUD.Variables = {
+	globalVars: {windowHeight: null},
+	properties: {
+		height: null,
+		start: null,
+		end: null
+	}
 };
 
-//changes loader string (best used to inform user of a new task being performed)
-function changeNKProgressHUD(loaderString) {  	
-	$('div.nkProgressHUDText').html(loaderString)
+//Shows HUD (Default string is 'Loading')
+nkProgressHUD.show = function(loaderString){
+	if (Modernizr.csstransforms3d){
+		console.log('show transform3d hud');
+	} else {
+		//append html
+		$('body').append("<div class='nkProgressHUDPage'><div class='nkProgressHUDWrapper'><div class='nkProgressHUDSpinner'></div><div class='nkProgressHUDText'>Loading</div></div></div>");
+
+		// set heights to vertically align HUD
+		nkProgressHUD.Variables.properties.height = $('.nkProgressHUDWrapper').height();	
+		nkProgressHUD.Variables.properties.start = ((nkProgressHUD.Variables.globalVars.windowHeight - nkProgressHUD.Variables.properties.height) / 2) - ((nkProgressHUD.Variables.globalVars.windowHeight / 100) * 5);
+		nkProgressHUD.Variables.properties.end = nkProgressHUD.Variables.properties.start + 50;
+		$('.nkProgressHUDWrapper').css('margin-top', nkProgressHUD.Variables.properties.end);
+
+		//start animation
+		$('.nkProgressHUDWrapper').show();
+		$('div.nkProgressHUDText').html(loaderString)
+		$('.nkProgressHUDWrapper').animate({marginTop: nkProgressHUD.Variables.properties.start,}, {duration: 1000,queue: false});
+		$('.nkProgressHUDPage').fadeIn();		
+	}		
 };
 
-//hides loader (best used at the end of a script)
-function dismissNKProgressHUD() {  	
-	$('.nkProgressHUDWrapper').animate({marginTop: nkProgressHUDWrapperEnd,}, {duration: 1000,queue: false});
-	setTimeout(function() {
-		$('.nkProgressHUDPage').fadeOut();
-	}, 460);
-	$('.nkProgressHUDWrapper').fadeOut();
-	//remove html
-	setTimeout(function() {
-		$('.nkProgressHUDPage').remove();
-	}, 1000);	
+
+
+//Changes HUD String (best used to inform user of a new task being performed)
+nkProgressHUD.change = function(loaderString){
+	if (Modernizr.csstransforms3d){
+		console.log('change transform3d hud');
+	} else {
+		$('div.nkProgressHUDText').html(loaderString)
+	}
 };
 
-//use demoProgressHUD to simulate loader animation prior to implementing
-function demoNKProgressHUD(loaderString) {  	
-	showNKProgressHUD(loaderString);
-	setTimeout(function() {
-		changeNKProgressHUD('Modify Message');
-	}, 2000);
-	setTimeout(function() {
-		changeNKProgressHUD('Dismiss HUD');
-	}, 3500);
-	setTimeout(function() {
-		dismissNKProgressHUD();
-	}, 5000);
+
+
+//Hides HUD (Best used at the end of a script)
+nkProgressHUD.dismiss = function(){
+	if (Modernizr.csstransforms3d){
+		console.log('dismiss transform3d hud');
+	} else {
+		$('.nkProgressHUDWrapper').animate({marginTop: nkProgressHUD.Variables.properties.end,}, {duration: 1000,queue: false});
+		setTimeout(function() {
+			$('.nkProgressHUDPage').fadeOut();
+		}, 460);
+		$('.nkProgressHUDWrapper').fadeOut();
+		//remove html
+		setTimeout(function() {
+			$('.nkProgressHUDPage').remove();
+		}, 1000);	
+	}  	
 };
 
-$(document).ready(function() {
-	windowHeight = $(window).height();
-});
 
-//resets window height when user resizes window
-$(window).resize(function() {
-	windowHeight = $(window).height(); 
-});
+//set windowHeight on load and resize.
+(function(){
+	nkProgressHUD.Variables.globalVars.windowHeight = $(window).height();
+	//resets window height when user resizes window
+	$(window).resize(function() {
+		nkProgressHUD.Variables.globalVars.windowHeight = $(window).height(); 
+	});
+}());
